60a0fd18623667bd0f91c78b249c0cd48b0024c9
Jason Montleon <jmontleo@redhat.com>
2016-03-11 14:13:03 -0500

    Additional updates for Satellite 6.2

--- ui/lib/fusor_ui/engine.rb
+++ ui/lib/fusor_ui/engine.rb
@@ -18,11 +18,15 @@ module FusorUi
 
         sub_menu :top_menu, :fusor_menu, :caption => N_('QuickStart Cloud Installer'), :after => :infrastructure_menu do
           menu :top_menu, :fusor_deployments,
+               :url      => '/r/#/deployments',
                :url_hash => { :controller => 'fusor_ui/placeholders', :action => :index },
-               :caption  => N_('Deployments')
+               :caption  => N_('Deployments'),
+               :engine   => FusorUi::Engine
           menu :top_menu, :new_fusor_deployment,
+               :url      => '/r/#/deployments/new/start',
                :url_hash => { :controller => 'fusor_ui/placeholders', :action => :new },
-               :caption  => N_('New Deployment')
+               :caption  => N_('New Deployment'),
+               :engine   => FusorUi::Engine
         end
 
       end
