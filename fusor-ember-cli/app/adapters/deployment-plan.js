f2640fba34969a2711b4967737f622529f1d6df1
Erik Nelson <erik@nsk.io>
2016-05-03 10:52:36 -0400

    Adapter indentation

--- fusor-ember-cli/app/adapters/deployment-plan.js
+++ fusor-ember-cli/app/adapters/deployment-plan.js
@@ -4,8 +4,8 @@ export default ApplicationAdapter.extend({
 
     // 'overcloud' is harded
     // ex. /fusor/api/openstack/deployments/:id/deployment_plans/overcloud
-    urlForFindRecord(id, modelName, snapshot) {
-        return '/fusor/api/openstack/deployments/' + id + '/deployment_plans/overcloud';
-    }
+  urlForFindRecord(id, modelName, snapshot) {
+    return '/fusor/api/openstack/deployments/' + id + '/deployment_plans/overcloud';
+  }
 
 });
