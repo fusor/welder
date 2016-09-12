1c231834e0bff3b38f2d60a4560c7a0a585786ce
Derek Whatley <dwhatley@redhat.com>
2016-09-12 18:26:57 -0400

    refactored cpu auto-detection code
    - added more comprehensive tests
    - added test fixtures
    - moved bulk of logic into util class
    - minimized database queries (thanks zeus)
    - fixed rubocop violations

--- server/app/controllers/fusor/api/v21/deployments_controller.rb
+++ server/app/controllers/fusor/api/v21/deployments_controller.rb
@@ -13,6 +13,7 @@
 require "net/http"
 require "sys/filesystem"
 require "uri"
+require "json"
 
 module Fusor
   class Api::V21::DeploymentsController < Api::V2::DeploymentsController
@@ -135,128 +136,11 @@ module Fusor
     end
 
     def compatible_cpu_families
-      # @deployment is provided by pre-filter run of find_deployment
-      rhv_hypervisors_list = @deployment.discovered_hosts
-
-      # find the fact_name_id corresponding to "processors"
-      processors_id = FactName.where(name: 'processors').first.id
-
-      # build string containing all cpu families attached to hypervisors
-      processor_concat = ''
-
-      rhv_hypervisors_list.each do |hypervisor|
-        processor_str = hypervisor.fact_values.where("fact_name_id" => processors_id).first.value
-        processor_hash = eval(processor_str)
-        processor_models = processor_hash["models"]
-        processor_concat << (processor_models.join(' ; ').downcase)
-      end
-
-      cpu_families_and_default = lcd_of_cpus(processor_concat)
-      render json: cpu_families_and_default, status: 200
+      rhv_hypervisors = @deployment.discovered_hosts
+      cpu_families = Utils::Fusor::CpuCompatDetector.rhv_cpu_families(rhv_hypervisors)
+      render json: cpu_families, status: 200
     end
 
-    def lcd_of_cpus(processor_concat)
-      # description: # finds oldest architecture, brand in rhv cluster
-
-      # input:  processor_concat = all processors in cluster as string
-      # output: a compatible list of cpu families will be returned oldest first.
-      # output: a best guess default cpu family to display
-
-      # if intel processors are being used, installed generation and older will be returned
-      # if amd processors are being used, all amd processors will returned
-      # if ibm processors are being used, all ibm processors will be returned
-      # if auto-detection doesn't match any processors, all options will be returned
-      cpu_fam_candidates = {
-        'intel': [
-          'Intel Conroe Family',
-          'Intel Penryn Family',
-          'Intel Nehalem Family',
-          'Intel Westmere Family',
-          'Intel SandyBridge Family',
-          'Intel Haswell-noTSX Family',
-          'Intel Haswell Family',
-        ],
-        'amd': [
-          'AMD Opteron G1',
-          'AMD Opteron G2',
-          'AMD Opteron G3',
-          'AMD Opteron G4',
-          'AMD Opteron G5'
-        ],
-        'ibm': [
-          'IBM POWER 8'
-        ]
-      }
-
-      # cpu family of cluster should match the oldest cpu present
-      # in the cluster, therefore newest processors are checked first
-      # and can be overwritten by older processor later in iteration
-      cpu_fams = [
-        # Intel processor families
-        { brand: 'intel', keywords: ['intel'], position: -1 }, #intel wildcard
-        { brand: 'intel', keywords: ['haswell'], position: 6 },
-        { brand: 'intel', keywords: ['haswell', 'no', 'tsx'], position: 5 },
-        { brand: 'intel', keywords: ['sandy', 'bridge'], position: 4 },
-        { brand: 'intel', keywords: ['westmere'], position: 3 },
-        { brand: 'intel', keywords: ['nehalem'], position: 2 },
-        { brand: 'intel', keywords: ['penryn'], position: 1 },
-        { brand: 'intel', keywords: ['conroe'], position: 0 },
-        # AMD processor families - no generational trimming
-        { brand: 'amd', keywords: ['amd'], position: -1 }, #amd wildcard
-        # IBM processor families
-        { brand: 'ibm', keywords: ['ibm'], position: -1 }, #ibm wildcard
-        { brand: 'ibm', keywords: ['ibm', 'power', '8'], position: 0 },
-      ]
-
-      # placeholder for best compatible cpu family for cluster
-      best_cpu_fam = { brand: 'none', keywords: [], value: -1 }
-
-      # iterate through cpu_fams looking for keyword matches
-      cpu_fams.each do |cpu_fam|
-        # if brand_X cpu found, break once we start checking brand_Z
-        if !(best_cpu_fam[:brand] == (cpu_fam[:brand]) || best_cpu_fam[:brand] == ('none'))
-          break
-        end
-        # ensure that all keywords match before setting new compatible best cpu family
-        if cpu_fam[:keywords].all? { |keyword| processor_concat.include? keyword }
-          best_cpu_fam = cpu_fam
-        end
-      end
-
-      if best_cpu_fam[:brand] == 'none'
-        # return all cpu families if keyword matches failed
-        all_cpu_fams = []
-        cpu_fam_candidates.values.each do |brand_fams| all_cpu_fams << brand_fams end
-        all_cpu_fams.flatten!
-        return {
-          cpu_families: all_cpu_fams,
-          default_family: 'Intel Nehalem Family'
-        }
-      else
-        # we were able to detect a brand
-        if best_cpu_fam[:position] == -1
-          # set default cpu family per brand
-          # this is used if a specific CPU family cannot be detected
-          case best_cpu_fam[:brand]
-          when 'intel'
-            default_fam = 'Intel Nehalem Family'
-          when 'amd'
-            default_fam = 'AMD Opteron G3'
-          when 'ibm'
-            default_fam = 'IBM POWER 8'
-          end
-        else
-          # if a known cpu family is detected in the cluster, set it as the default
-          default_fam = cpu_fam_candidates[best_cpu_fam[:brand].to_sym][best_cpu_fam[:position]]
-        end
-
-        return {
-          # keep all families from best_cpu_fam and older
-          cpu_families: cpu_fam_candidates[best_cpu_fam[:brand].to_sym][0..best_cpu_fam[:position]],
-          default_family: default_fam
-        }
-      end
-    end
 
     def check_mount_point
       mount_address = params['address']
--- server/test/controllers/deployments_controller_test.rb
+++ server/test/controllers/deployments_controller_test.rb
@@ -515,59 +515,13 @@ module Fusor
       end
     end
 
-    context 'lcd_of_cpus' do
-      test 'lcd_of_cpus should return all cpus if no known brands found in cluster cpu list' do
-        processor_concat = 'unlisted brand quad core processor ; ' * 4
-        cpu_families_and_default = @controller.lcd_of_cpus(processor_concat)
-        cpu_families = cpu_families_and_default[:cpu_families]
-        default_family = cpu_families_and_default[:default_family]
-        cpu_families_concat = cpu_families.join(" ; ")
-        assert cpu_families_concat.downcase.include?('intel')
-        assert cpu_families_concat.downcase.include?('amd')
-        assert cpu_families_concat.downcase.include?('ibm')
-        assert default_family.downcase.include?('intel nehalem')
-      end
-
-      test 'lcd_of_cpus should return all intel cpus if "intel" found, but known arch not found' do
-        processor_concat = 'intel core processor (baybridge, not_real) ; ' * 4
-        cpu_families_and_default = @controller.lcd_of_cpus(processor_concat)
-        cpu_families = cpu_families_and_default[:cpu_families]
-        cpu_families.each do |cpu_fam| assert cpu_fam.downcase.include?('intel') end
-      end
-
-      test 'lcd_of_cpus should return all amd cpus if "amd" found, but known arch not found' do
-        processor_concat = 'amd opteron(tm) processor 6172 ; ' * 4
-        processor_concat << 'amd opteron(tm) processor 4362 ; ' * 4
-        cpu_families_and_default = @controller.lcd_of_cpus(processor_concat)
-        cpu_families = cpu_families_and_default[:cpu_families]
-        cpu_families.each do |cpu_fam| assert cpu_fam.downcase.include?('amd') end
-      end
-
-      test 'lcd_of_cpus should return all ibm cpus if "ibm" found, but known arch not found' do
-        processor_concat = 'ibm powerpc processor ; ' * 4
-        cpu_families_and_default = @controller.lcd_of_cpus(processor_concat)
-        cpu_families = cpu_families_and_default[:cpu_families]
-        cpu_families.each do |cpu_fam| assert cpu_fam.downcase.include?('ibm') end
-      end
-
-      test 'lcd_of_cpus should return haswell-notsx and older if entire cluster is haswell-notsx' do
-        processor_concat = 'intel core processor (haswell, no tsx) ; ' * 4
-        cpu_families_and_default = @controller.lcd_of_cpus(processor_concat)
-        cpu_families = cpu_families_and_default[:cpu_families]
-        default_family = cpu_families_and_default[:default_family]
-        assert default_family.downcase.include?('notsx')
-        assert cpu_families.count == 6
-        cpu_families.each do |cpu_fam| assert cpu_fam.downcase.include?('intel') end
-      end
-
-      test 'lcd_of_cpus should return nehalem, penryn, and conroe if sandybridge and nehalem cpus in cluster' do
-        processor_concat = 'intel core processor (sandybridge, no tsx) ; ' * 4
-        processor_concat << 'intel core processor (nehalem, no tsx) ; ' * 4
-        cpu_families_and_default = @controller.lcd_of_cpus(processor_concat)
-        cpu_families = cpu_families_and_default[:cpu_families]
-        default_family = cpu_families_and_default[:default_family]
-        assert cpu_families.count == 3
-        assert default_family.downcase.include?('nehalem')
+    context 'cpu_compat_detector' do
+      test 'compatible_cpu_families should return a compatible list of cpus' do
+        @deployment.discovered_hosts = [hosts(:host_haswell)]
+        response = JSON.parse(get(:compatible_cpu_families, :id => @deployment.id).body)
+        assert_response :success
+        assert response['cpu_families'].count == 6
+        assert response['default_family'].include?('Haswell-noTSX')
       end
     end
   end
