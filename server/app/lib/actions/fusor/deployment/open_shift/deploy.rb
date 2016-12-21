a6d8766291acc05a6c66ee8ca8d46f29108c25e2
Derek Whatley <derekwhatley@gmail.com>
2016-12-21 17:36:29 -0500

    Launch OCP VMs using Ansible

--- server/app/lib/actions/fusor/deployment/open_shift/deploy.rb
+++ server/app/lib/actions/fusor/deployment/open_shift/deploy.rb
@@ -26,12 +26,6 @@ module Actions
               plan_action(::Actions::Fusor::Deployment::Rhev::OseLaunch,
                           deployment)
 
-              plan_action(::Actions::Fusor::Host::WaitUntilOseProvisioned,
-                          deployment.id)
-
-              plan_action(::Actions::Fusor::Deployment::OpenShift::WaitForSshd,
-                          deployment)
-
               plan_action(::Actions::Fusor::Deployment::OpenShift::CopySshKey,
                           deployment, 'rsa')
 
